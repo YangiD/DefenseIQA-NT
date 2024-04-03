44938794e85899334ea403468c8377a6d2bbb22d
YangiD <61107470+YangiD@users.noreply.github.com>
2024-04-03 11:25:40 +0800

    Update quality_and_norm_demo.py
    Add regenerate_crops option to generate crops for a new image.

--- quality_and_norm_demo.py
+++ quality_and_norm_demo.py
@@ -10,6 +10,7 @@ import argparse
 from torch.autograd import grad
 from torch.autograd import Variable
 from FGSM_demo import norm
+import random
 
 
 def pil_loader(path):
@@ -26,7 +27,26 @@ def fix_seed(seed):
         torch.backends.cudnn.deterministic = True
     np.random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
+    random.seed(seed)
 
+
+def generate_save_patches(img_path,patch_num,save_dir,patch_size=224):
+    assert os.path.exists(img_path), 'Image path is invaild, please check image path or the choice of re_generation option.'
+    img = pil_loader(img_path)
+    imgname = img_path.split('/')[-1].split('.')[0]
+    W,H = img.size
+    assert W>=patch_size and H>=patch_size, 'The size of image should be larger than or equal to 224*224.'
+    for j in range(patch_num):
+        rand_W = random.randint(0,W-patch_size)
+        rand_H = random.randint(0,H-patch_size)
+        img_crop = img.crop((rand_W,rand_H,rand_W+patch_size,rand_H+patch_size))
+        newname = imgname + '_' + str(j) + '.bmp'
+        assert not os.path.exists(os.path.join(save_dir,newname)), 'Existed:'+os.path.join(save_dir,newname)
+        img_crop.save(os.path.join(save_dir,newname))
+    print('Patches saved to:', save_dir)
+    return imgname
+    
+    
 def main(config):
     fix_seed(919)
     crop_dir = './images_fixedcrop'
@@ -35,7 +55,7 @@ def main(config):
     # HyperIQA
     model_hyper = models.HyperNet(16, 112, 224, 112, 56, 28, 14, 7).cuda()
     model_hyper.train(False)
-    modelpath = './checkpoints/livec_bs16_grad[0]_weight[0.0].pth'
+    modelpath = './checkpoints/livec_bs16_grad[0]_weight[0.0]_2.pth'
     model_dict = torch.load(modelpath)
     if 'model' in model_dict:
         model_hyper.load_state_dict(model_dict['model'])
@@ -44,7 +64,7 @@ def main(config):
     # HyperIQA+NT
     model_hyper_NT = models.HyperNet(16, 112, 224, 112, 56, 28, 14, 7).cuda()
     model_hyper_NT.train(False)
-    modelpath = './checkpoints/livec_bs16_grad[1]_weight[0.001]_h[0.01].pth'
+    modelpath = './checkpoints/livec_bs16_grad[1]_weight[0.001]_2.pth' #_h[0.01].pth
     model_dict_NT = torch.load(modelpath)
     if 'model' in model_dict:
         model_hyper_NT.load_state_dict(model_dict_NT['model'])
@@ -57,17 +77,22 @@ def main(config):
                     torchvision.transforms.Normalize(mean=(0.485, 0.456, 0.406),
                                                      std=(0.229, 0.224, 0.225))])
     
-    imgname = config.img_name
+    imgname = config.img
     norm_hyperIQA = []
     norm_hyperIQA_NT = []
     ori_score = []
     NT_score = []
-    # 25 patches for each original image
+
+    # generate 25 patches for each original image
+    if config.regenerate_crops:
+        imgname = generate_save_patches(config.img,config.patch_size,crop_dir,patch_size=224)
+        
+
     for i in range(config.patch_size):
         ori_normi = None
         NT_normi = None
 
-        imgpath = os.path.join(crop_dir,imgname[:-4]+'_'+str(i)+imgname[-4:])
+        imgpath = os.path.join(crop_dir,imgname.split('.')[0]+'_'+str(i)+'.bmp')
         img = pil_loader(imgpath)
         img = transforms(img)
         img = torch.tensor(img.cuda()).unsqueeze(0)
@@ -115,10 +140,10 @@ def main(config):
     ori_score = np.array(ori_score).mean()
     NT_score = np.array(NT_score).mean()
 
-    print('For {}, the L_1 norm of output\'s gradint in term of the input image:'.format(config.img_name))
+    print('For {}, the L_1 norm of output\'s gradint in term of the input image:'.format(config.img))
     print('HyperIQA:{:.4f}'.format(norm_hyperIQA))
     print('HyperIQA+NT:{:.4f}'.format(norm_hyperIQA_NT))
-    print('For {}, the predicted score of the image:'.format(config.img_name))
+    print('For {}, the predicted score of the image:'.format(config.img))
     print('HyperIQA:{:.4f}'.format(ori_score))
     print('HyperIQA+NT:{:.4f}'.format(NT_score))
 
@@ -126,6 +151,7 @@ def main(config):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--patch_size', type=int, default=25, help='optional patch size: 10, 25')
-    parser.add_argument('--img_name', type=str, default='123.bmp', help='optional image name in this demo: 123.bmp, 447.bmp')
+    parser.add_argument('--img', type=str, default='123.bmp', help='optional image name in this demo: 123.bmp, 447.bmp, or other path to one other image')
+    parser.add_argument('--regenerate_crops', action='store_true', help='if true, the input image is croped to 224*224 patches')
     config = parser.parse_args()
     main(config)
